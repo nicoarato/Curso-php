687814e1c75d8e13126106d72e0b9695f7089927
Nico Arato <nico.arato@gmail.com>
2019-04-23 12:39:53 -0300

    06-operadores

---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,31 @@
+<?php
+
+//operadores aritmeticos
+$num1 = 55;
+$num2 = 33;
+
+echo "<p>Suma: " . ($num1+ $num2) . "</p>";
+echo "<p>Resta: " . ($num1- $num2) . "</p>";
+echo "<p>Multiplicación: " . ($num1*$num2) . "</p>";
+echo "<p>División: " . ($num1/ $num2) . "</p>";
+echo "<p>Modulo: " . ($num1%$num2) . "</p>";
+
+//Operadores de incremento y decremento
+
+$year= 2019;
+
+$year++;
+$year--;
+
+--$year;
+++$year;
+echo "<h1>$year</h1>";
+
+
+
+
+
+
+
+
+?>
