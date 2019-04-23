4b26c63b6d84b3c32f9811ecac2ad5fbec0996a1
Nico Arato <nico.arato@gmail.com>
2019-04-23 13:24:41 -0300

    07-superglobales
    _GET y _POST

---
+++ aprendiendo-php/07-superglobales/index.php
@@ -0,0 +1,28 @@
+<?php
+    //Variables de servidor
+
+    //IP desde donde sale el servidor
+    echo "<h1>". $_SERVER['SERVER_ADDR'] . "</h1>";
+
+    //nombre del dominio
+    echo "<h1>". $_SERVER['SERVER_NAME'] . "</h1>";
+
+    //nombre del software que usa el servidor
+    echo "<h1>". $_SERVER['SERVER_SOFTWARE'] . "</h1>";
+
+    //navegador que estoy usando
+    echo "<h1>". $_SERVER['HTTP_USER_AGENT'] . "</h1>";
+
+    //ip del cliente
+    echo "<h1>". $_SERVER['REMOTE_ADDR'] . "</h1>";
+
+
+    //Variables GET
+    
+
+
+
+
+
+
+?>
