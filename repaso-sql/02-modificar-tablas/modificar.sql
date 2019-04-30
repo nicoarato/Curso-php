aa3d15d9d0a8c26676629637f5c5cf34c2b96447
Nico Arato <nico.arato@gmail.com>
2019-04-30 13:59:11 -0300

    Modificadores

---
+++ repaso-sql/02-modificar-tablas/modificar.sql
@@ -0,0 +1,19 @@
+#Renombrar una tabla#
+
+ALTER TABLE usuarios RENAME TO usuarios_renom;
+
+#Cambiar nombre de una columna
+
+ALTER TABLE usuarios_renom CHANGE apellidos apellido VARCHAR(100) NULL; 
+
+#modificar columna sin cambiar columna
+
+ALTER TABLE usuarios_renom MODIFY apellido VARCHAR(40) NOT NULL;
+
+#añadir columna
+
+ALTER TABLE usuarios_renom ADD website VARCHAR(100) null;
+
+# añadir restricción a columna
+
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
