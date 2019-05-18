5ac5b30a6189aa04f387a0d6b1434cf41e1f158a
Nico Arato <nico.arato@gmail.com>
2019-05-18 18:47:21 -0300

    Generación de PDF con html2pdf.

---
+++ aprendiendo-librerias-php/generar_pdf/pdf_para_generar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Generar PDF</title>
+    <style type="text/css">
+        body{
+            font-size: 14px;
+            font-family: 'Lato', 'Arial';
+        }
+        h1{
+            background: #c1c1c1;
+        }
+        ul{
+            list-style: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola mundo desde una libreria php: HTML2PDF</h1>
+    <p>Curso de PHP 2019</p>
+    <ul>
+        <li>PHP y Mysql</li>
+        <li>javascript</li>
+        <li>Full Stack</li>
+        <li>Aprendiendo Synfony</li>
+
+    
+    </ul>
+</body>
+</html>
---
+++ aprendiendo-librerias-php/index.php
@@ -0,0 +1,19 @@
+<?php
+require 'vendor/autoload.php';
+
+use  Spipu\Html2Pdf\Html2Pdf;
+$html2pdf = new Html2Pdf();
+
+/*
+$html= '<h1>Hola mundo desde una libreria php</h1>';
+$html .= "<p>Esto es una prueba de creación de pdf.</p>";
+*/
+
+//Recoger la vista a imprimir
+ob_start();
+require_once 'generar_pdf/pdf_para_generar.php';
+$html= ob_get_clean();
+
+$html2pdf->writeHTML($html);
+//$html2pdf->output();
+$html2pdf->output('archivo.pdf');
