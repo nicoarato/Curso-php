a162de455870337f2a595441c834614dc794e5ff
Nico Arato <nico.arato@gmail.com>
2019-10-01 14:12:42 -0300

    Controlador de images.
    php artisan make:controller ImageController
    Se crea formulario para subir imagen.

---
+++ clon-instagram-laravel/app/Http/Controllers/ImageController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+
+class ImageController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function create(){
+        return view('image.create');
+    }
+    
+    public function save(Request $request){
+        //validacion
+        $validate = $this->validate($request, [
+            'description' => 'required',
+            'image_path' => 'required|image'
+        ]);
+        
+        //recoger datos
+        $image_path = $request->file('image_path');
+        $description = $request->input('description');
+        
+        //Asignar valores a nuevo objeto
+        $user = \Auth::user();
+        $image = new Image;
+        $image->user_id = $user->id;
+        $image->description = $description;
+        
+        //subir fichero
+        if($image_path){
+            $image_path_name = time().$image_path->getClientOriginalName();
+            Storage::disk('images')->put($image_path_name,File::get($image_path));
+            $image->image_path= $image_path_name;
+        }
+        $image->save();
+        
+        return redirect()->route('home')->with([
+            'message' => 'Foto subida con Éxito'
+        ]);
+        
+    }
+}
--- clon-instagram-laravel/app/Http/Controllers/UserController.php
+++ clon-instagram-laravel/app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\File; //para el objeto storage
 class UserController extends Controller
 {
     
-     public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
