3c6c75ece93ce5c563b01e2cb45ff867f7bc5552
Nico Arato <nico.arato@gmail.com>
2019-10-14 02:13:08 -0300

    Guardamos comentarios.

--- clon-instagram-laravel/app/Http/Controllers/CommentController.php
+++ clon-instagram-laravel/app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
 
 class CommentController extends Controller
 {
@@ -12,13 +13,34 @@ class CommentController extends Controller
     }
     
     public function save(Request $request){
+        
+        //validar datos
         $validate = $this->validate($request, [
             'image_id' => 'integer|required',
             'content' => 'string|required',
         ]);
         
+        //recoger datos
+        $user = \Auth::user();
         $image_id = $request->input('image_id');
         $content = $request->input('content');
+       
+        //Asigno valores a nuevo objeto
+        $comment = new Comment();
+        
+        $comment->user_id = $user->id;
+        $comment->image_id = $image_id;
+        $comment->content = $content;
+        
+        //guardo en bdatos
+        $comment->save();
+        
+        //redireccion
+        return redirect()->route('image.detail',['id' => $image_id])
+                         ->with([
+                             'message' => 'Has comentado correctamente'
+                         ])
+                ;
     }
     
     
